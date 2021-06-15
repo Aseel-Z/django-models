9cd71cce836a4e82abc2ba6220045c4b10c04cc9
Aseel-Z <aseelalzweri@gmail.com>
2021-06-15 09:57:05 +0300

    ---3---

--- snacks/tests.py
+++ snacks/tests.py
@@ -2,23 +2,23 @@ from django.test import TestCase
 from django.urls import reverse
 
 class thingsTests(TestCase):
-    def test_home_page_status_code(self):
+    def test_snack_list_page_status_code(self):
         url = reverse('snack_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_templete(self):
+    def test_snack_list_page_templete(self):
         url = reverse('snack_list')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'snack_list.html')
         self.assertTemplateUsed(response, 'base.html')
 
-    def test_about_page_status_code(self):
+    def test_snack_detail_page_status_code(self):
         url = reverse('snack_detail')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_about_page_templete(self):
+    def test_snack_detail_page_templete(self):
         url = reverse('snack_detail')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'snack_detail.html')
--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import SnackListView, SnackDetailView
-
+# name from test
 urlpatterns = [
     path('', SnackListView.as_view(), name="snack_list"),
     path('<int:pk>/', SnackDetailView.as_view(), name="snack_detail"),
